16710702e130f34e5197b093a2bf0b4542fd527e
stephenywc <stephenyw3@gmail.com>
2025-09-18 16:22:26 +0800

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+package dit1334_ood_firstProject; //my Package name
+
+//Author name: Chong Yuen Wei
+//Date create: 11.09.2025
+//Purpose of code: To create  First Java Project
+
+//library that being imported when necessary
+
+public class helloWorld { //"public class" is fixed, but the "helloWorld" can be changed as your wish because it is the name of your class
+	public static void main (String[] args) { // this line is compulsory
+		System.out.println("Hello World");
+		System.out.println("This is my First Project");
+		System.out.print("I'm proud with my code");
+	}// this line is compulsory to complete the public static void main's "{"
+}//this line is compulsory to complete the public class's "{"
